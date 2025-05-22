a185605461fe17fc1a964538a1495f78147efda5
gael <febgael16@gmail.com>
2025-05-22 10:53:54 +0300

    mini firewall test

---
+++ firewall.py
@@ -0,0 +1,28 @@
+import random
+
+def generate_random_ip():
+	return f"192.168.1.{random.randint(0,20)}" #to generate random ip address
+
+def check_fireRules(ip_Add,fireRules):
+	for rule_ip, action in fireRules.items():
+		if ip_Add==rule_ip:
+			return action
+	return "BLOCK"
+
+def main():
+	fireRules={
+		"192.168.1.1":"ALLOW", #list of allowed ip addresses
+		"192.168.1.4":"ALLOW",
+		"192.168.1.6":"ALLOW",
+		"192.168.1.13":"ALLOW",
+		"192.168.1.16":"ALLOW",
+		"192.168.1.9":"ALLOW"
+		}
+	for _ in range(12):  #generates 12 ip address
+		ip_Add=generate_random_ip()
+		action=check_fireRules(ip_Add,fireRules)
+		randNum=random.randint(0,9999) #unique id to differentiate
+		print(f"IP: {ip_Add}, Action:{action}, Random: {randNum}")
+if __name__== "__main__":
+	main()
+
